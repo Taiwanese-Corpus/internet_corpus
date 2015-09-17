f509a349b25c913647db9c42d3c9dc4fb6ae37fe
薛丞宏 <ihcaoe@gmail.com>
2015-09-17 21:23:44 +0800

    試用yaml

---
+++ README.md
@@ -0,0 +1,11 @@
+# 網路語料
+
+整理隨手抓的小量語料，若需自動化大量處理，另開專案
+
+## 產生yaml
+```bash
+virtualenv --python=python3 venv
+. venv/bin/activate
+pip install pyyaml
+python 範例資料.py 
+```
--- 範例資料.py
+++ 範例資料.py
@@ -1,3 +1,6 @@
+from itertools import zip_longest
+
+import yaml
 
 
 夜市資料 = {
@@ -157,3 +160,36 @@
         ]
     },
 ]
+
+
+# print(yaml.dump(夜市資料, default_flow_style=False, allow_unicode=True))
+夜市輸出資料 = {
+    '版權': '無版權',
+    '種類': '語句',
+    '語言腔口': '閩南語',
+    '著作所在地': '臺灣',
+    '著作年': '2015',
+    '下層': [],
+}
+for 規組語句資料 in 夜市資料['資料']:
+    頂層 = 夜市輸出資料
+    for 第幾層, (來源, 語句資料) in enumerate(zip_longest(夜市資料['來源'], 規組語句資料)):
+        if 第幾層 == 0:
+            新資料 = {
+                '來源': 來源,
+                '外語語言': '華語',
+                '外語資料': 語句資料,
+                '下層': [],
+            }
+        else:
+            新資料 = {
+                '來源': 來源,
+                '文本資料': 語句資料,
+                '下層': [],
+            }
+        頂層['下層'].append(新資料)
+        頂層 = 新資料
+
+print(yaml.dump(夜市輸出資料, default_flow_style=False, allow_unicode=True))
+with open('柱柱姊掃街變成來亂的.yaml','w') as 檔案:
+    yaml.dump(夜市輸出資料,檔案, default_flow_style=False, allow_unicode=True)
