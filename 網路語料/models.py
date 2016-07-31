a364bc5b7a63280ae6280a86428979bef40fa7f8
薛丞宏 <ihcaoe@gmail.com>
2016-07-31 13:32:55 +0800

    加文本預設來源 #1

--- 網路語料/models.py
+++ 網路語料/models.py
@@ -2,6 +2,32 @@ import json
 
 from django.db import models
 from django.forms.models import ModelForm
+from django.core.exceptions import ValidationError
+
+
+預設來源 = json.dumps({
+    '名': '',
+    '網址': 'http://',
+}, indent=2, ensure_ascii=False, sort_keys=True)
+
+
+def 愛有名(value):
+    try:
+        資料 = json.loads(value)
+    except:
+        raise ValidationError(
+            ('愛是json'),
+        )
+    try:
+        名 = 資料['名']
+    except:
+        raise ValidationError(
+            ('愛有名'),
+        )
+    if 名 == '':
+        raise ValidationError(
+            ('名袂使是空的'),
+        )
 
 
 class 語料表(models.Model):
@@ -11,13 +37,10 @@ class 語料表(models.Model):
     華語內容 = models.TextField(blank=True)
     華語來源 = models.TextField(blank=True)
     臺語內容 = models.TextField()
-    臺語來源 = models.TextField(default=json.dumps({
-        '名': '',
-        '網址': 'http://',
-    }, indent=2, ensure_ascii=False, sort_keys=True))
+    臺語來源 = models.TextField(default=預設來源, validators=[愛有名])
     文本內容 = models.TextField()
     音標內容 = models.TextField()
-    文本音標來源 = models.TextField()
+    文本音標來源 = models.TextField(default=預設來源, validators=[愛有名])
 
     備註 = models.CharField(max_length=200, blank=True)
 
